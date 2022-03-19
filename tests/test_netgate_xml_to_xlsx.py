a756a7dbaa613ad5e193618a09b6e0b0d92b7199
Raymond GA Côté <ray@AppropriateSolutions.com>
2022-03-19 16:11:33 -0400

    Move script into package.

--- tests/test_netgate_xml_to_xlsx.py
+++ tests/test_netgate_xml_to_xlsx.py
@@ -1,5 +1,9 @@
+"""Netgate module testing."""
+# Copyright © 2022 Appropriate Solutions, Inc. All rights reserved.
+
 from netgate_xml_to_xlsx import __version__
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    """Placeholder while building out tests."""
+    assert __version__ == "0.9.1a1"
