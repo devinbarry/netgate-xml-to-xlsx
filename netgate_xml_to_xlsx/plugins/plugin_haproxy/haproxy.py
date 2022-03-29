bedc1b0c7fe3dd6db7a22da5f9a4387d60889584
Raymond GA Côté <ray@AppropriateSolutions.com>
2022-03-29 12:47:09 -0400

    HAProxy Backends

--- netgate_xml_to_xlsx/plugins/plugin_haproxy/haproxy.py
+++ netgate_xml_to_xlsx/plugins/plugin_haproxy/haproxy.py
@@ -29,11 +29,61 @@ def _haproxy_overview(nodes: OrderedDict) -> SheetData:
     rows = load_standard_nodes(nodes=nodes, field_names=field_names)
     rows = list(zip(field_names, rows[0]))
 
-    return SheetData(
+    yield SheetData(
         sheet_name="HAProxy",
         header_row=split_commas("Name,Value"),
         data_rows=rows,
         column_widths=split_commas(column_widths),
+    )
+
+
+def _haproxy_backends(nodes: OrderedDict | list[OrderedDict]) -> SheetData:
+    """HAProxy backends can have 1 or more items."""
+    rows = []
+    # Add a_extaddr before returning
+    field_names = split_commas(
+        "name,status,type,primary_frontend,backend_serverpool,"  # 5
+        "forwardfor,dontlognull,log-detailed,socket-stats,a_extaddr,"  # 10
+        "ha_certificate,clientcert_ca,clientcert_crl,a_actionitems,a_errorfiles,"  # 15
+        "dcertadv,ssloffloadcert,advanced,ha_acls,httpclose"  # 20
+    )
+
+    column_widths = split_commas(
+        "20,20,20,25,25,20,20,20,20,40,20,20,20,20,20,80,20,20,20,20,20"
+    )
+
+    if isinstance(nodes, OrderedDict):
+        nodes = [nodes]
+
+    for node in nodes:
+        row = load_standard_nodes(nodes=node, field_names=field_names)
+        assert len(row) == 1
+        row_dict = dict(zip(field_names, row[0]))
+
+        a_extaddr = ""
+        a_extaddr_nodes = get_element(node, "a_extaddr,item")
+        if a_extaddr_nodes:
+            a_ext_rows = load_standard_nodes(
+                nodes=a_extaddr_nodes,
+                field_names=split_commas("extaddr,extaddr_port,extaddr_ssl,_index"),
+            )
+            assert len(a_ext_rows) == 1
+            a_extaddr += "\n".join(
+                [
+                    f"{x}: {y}"
+                    for x, y in list(
+                        zip("addr,port,ssl,_index".split(","), a_ext_rows[0])
+                    )
+                ]
+            )
+        row_dict["a_extaddr"] = a_extaddr + "\n"
+        rows.append([row_dict[key] for key in field_names])
+
+    yield SheetData(
+        sheet_name="HAProxy Backends",
+        header_row=field_names,
+        data_rows=rows,
+        column_widths=column_widths,
     )
 
 
@@ -60,6 +110,10 @@ class Plugin(BasePlugin):
         """Document unbound elements."""
         super().run(pfsense)
 
-        nodes = get_element(pfsense, "installedpackages,haproxy")
+        haproxy = get_element(pfsense, "installedpackages,haproxy")
+        for overview in _haproxy_overview(haproxy):
+            yield overview
 
-        yield _haproxy_overview(nodes)
+        backends = get_element(haproxy, "ha_backends,item")
+        for backend in _haproxy_backends(backends):
+            yield backend
