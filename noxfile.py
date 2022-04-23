5e6304ed5f62854ff499745f0dc936cc9e8510d4
Raymond GA Côté <ray@AppropriateSolutions.com>
2022-04-22 20:56:02 -0400

    nox: automation wip

--- noxfile.py
+++ noxfile.py
@@ -9,12 +9,13 @@ from textwrap import dedent
 
 import nox
 
+package = "netgate_xml_to_xlsx"
 python_versions = [
     "3.9",
 ]
 nox.options.reuse_existing_virtualenv = True
 nox.needs_version = ">= 2022.1.7"
-nox.options.sessions = ("format",)
+nox.options.sessions = ("flake8",)
 
 
 try:
@@ -35,3 +36,39 @@ def format(session: Session) -> None:
     session.install("black", "isort")
     session.run("isort", ".")
     session.run("black", ".")
+
+
+@session(name="flake8", python=python_versions)
+def flake8(session: Session) -> None:
+    """Simple flake8 check.
+
+    Uses pyproject-flake8 which is a monkey-patched version that reads from pyproject.toml
+    Format before flaking.
+    """
+    format(session)
+    # session.notify("format")
+    session.install("pyproject-flake8")
+    session.run("pflake8", package)
+
+
+@session(name="pylint", python=python_versions)
+def pylint(session: Session) -> None:
+    """Standard pylint. Format before linting."""
+    format(session)
+    session.install("pylint")
+    session.run("pylint", "--rcfile", "pylint.rc", package)
+
+
+@session(name="mypy", python=python_versions)
+def mypy(session: Session) -> None:
+    """Type check the module."""
+    session.install("mypy")
+    session.run("mypy", "-p", package)
+
+
+@session(name="security", python=python_versions[0])
+def security(session: Session) -> None:
+    """Standard security checks."""
+    session.install("bandit", "safety")
+    session.run("bandit", ".")
+    session.run("safety", "check")
