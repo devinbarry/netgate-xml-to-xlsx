4d310a88573546ad35b5c751a8eb939d1db5f1ba
Raymond GA Côté <ray@AppropriateSolutions.com>
2022-05-31 13:36:39 -0400

    More OpenVPN Export nodes

--- src/netgate_xml_to_xlsx/plugins/plugin_installed_vpn_openvpn_export/vpn_openvpn_export.py
+++ src/netgate_xml_to_xlsx/plugins/plugin_installed_vpn_openvpn_export/vpn_openvpn_export.py
@@ -6,9 +6,15 @@ from typing import Generator
 from netgate_xml_to_xlsx.mytypes import Node
 
 from ..base_plugin import BasePlugin, SheetData
-from ..support.elements import xml_findone
+from ..support.elements import xml_findall, xml_findone
 
-NODE_NAMES = "config"
+NODE_NAMES = (
+    "node,advancedoptions,blockoutsidedns,legacy,pass,"
+    "pkcs11providers,proxyaddr,proxypass,proxyport,proxyuser,"
+    "randomlocalport,server,useaddr,useaddr_hostname,usepass,"
+    "usepkcs11,useproxy,useproxypass,useproxytype,usetoken,"
+    "verifyservercn"
+)
 
 
 class Plugin(BasePlugin):
@@ -22,20 +28,55 @@ class Plugin(BasePlugin):
         """Initialize."""
         super().__init__(display_name, node_names)
 
+    def adjust_node(self, node: Node) -> str:
+        """Local node adjustments."""
+        if node is None:
+            return ""
+
+        return super().adjust_node(node)
+
     def run(self, parsed_xml: Node) -> Generator[SheetData, None, None]:
         """Gather information."""
-        node = xml_findone(parsed_xml, "installedpackages/vpn_openvpn_export")
-        if node is None:
+        rows = []
+        vpn_node = xml_findone(parsed_xml, "installedpackages/vpn_openvpn_export")
+        if vpn_node is None:
             return
 
-        if self.report_unknown_node_elements(node):
-            return
+        self.report_unknown_node_elements(
+            vpn_node, "defaultsettings,serverconfig".split(",")
+        )
 
-        # Our samples show bare config.
-        node = xml_findone(node, "config")
-        if node is None:
-            return
+        # Default settings.
+        node = xml_findone(vpn_node, "defaultsettings")
+        if node is not None:
+            self.report_unknown_node_elements(node)
+            row = []
+            for node_name in self.node_names:
+                if node_name == "node":
+                    row.append("defaultsettings")
+                    continue
 
-        self.report_unknown_node_elements(node, [])
+                row.append(self.adjust_node(xml_findone(node, node_name)))
+            rows.append(self.sanity_check_node_row(node, row))
 
-        yield None
+        # Serverconfig. Allowing there to be more than one.
+        nodes = xml_findall(vpn_node, "serverconfig,item")
+        if nodes is not None:
+            for node in nodes:
+                self.report_unknown_node_elements(node)
+                row = []
+                for node_name in self.node_names:
+                    if node_name == "node":
+                        row.append("serverconfig")
+                        continue
+
+                    row.append(self.adjust_node(xml_findone(node, node_name)))
+                rows.append(self.sanity_check_node_row(node, row))
+
+        yield self.rotate_rows(
+            SheetData(
+                sheet_name=self.display_name,
+                header_row=self.node_names,
+                data_rows=rows,
+            )
+        )
